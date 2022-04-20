e5c213cd303e02a7d680a673c4258ddb2d53a281
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-04-20 21:07:14 +0300

    add deserialization/serialization DRF

--- enrollment/views.py
+++ enrollment/views.py
@@ -1,26 +1,38 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
+from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ListField
 
 from .models import Application, Participant
 
 
+class ParticipantSerializer(ModelSerializer):
+    class Meta:
+        model = Participant
+        fields = ['first_name', 'last_name', 'email']
+
+
+class ApplicationSerializer(ModelSerializer):
+    participants = ParticipantSerializer(many=True)
+
+    class Meta:
+        model = Application
+        fields = ['contact_phone', 'ticket_type', 'participants']
+
+
 @api_view(['POST'])
 def enroll(request):
-    if 'contact_phone' not in request.data:
-        return Response(['Contact phone field is required.'], status=400)
-
-    if 'ticket_type' not in request.data:
-        return Response(['Ticket type field is required.'], status=400)
-    # TODO check if ticket_type is one of available choices
-    
-    participants = request.data.get('participants', [])  # TODO validate data!
+    serializer = ApplicationSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
 
     application = Application.objects.create(
-        contact_phone=str(request.data['contact_phone']),
-        ticket_type=str(request.data['ticket_type']),
+        contact_phone=serializer.validated_data['contact_phone'],
+        ticket_type=serializer.validated_data['ticket_type'],
     )
 
-    participants = [Participant(application=application, **fields) for fields in participants]
+    participants_fields = serializer.validated_data['participants']
+    participants = [Participant(application=application, **fields) for fields in participants_fields]
     Participant.objects.bulk_create(participants)
 
     return Response({
